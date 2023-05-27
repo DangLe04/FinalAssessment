19664b40019c7541364a8dfecfd8881c911db79a
thaiducpro <nguyenhuuthai.pro@gmail.com>
2023-05-28 02:05:54 +1000

    update

---
+++ src/Model/Document.java
@@ -0,0 +1,52 @@
+package Model;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "document")
+@NamedQueries({
+        @NamedQuery(name = "Document.findByDocumentId", query = "select d from Document d where d.documentId = :documentId")
+})
+public class Document{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int documentId;
+    private String title;
+    private String content;
+
+    public Document(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+    public Document() {
+
+    }
+
+    public int getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(int id) {
+        this.documentId = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
